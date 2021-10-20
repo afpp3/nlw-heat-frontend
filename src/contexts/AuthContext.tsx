38bd45cc83f8713bef50d2c1a3e0d30eb5b5bcf0
Andre <afelipp3@gmail.com>
2021-10-20 18:45:44 -0300

    Create AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,85 @@
+import { createContext, useEffect, useState } from 'react'
+import { api } from '../services/api'
+
+type User = {
+  id: string
+  name: string
+  login: string
+  avatar_url: string
+}
+
+type AuthContextData = {
+  user: User | null
+  signInUrl: string
+  signOut: () => void
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+type AuthProviderProps = {
+  children: React.ReactNode
+}
+
+type AuthResponse = {
+  token: string
+  user: {
+    id: string
+    avatar_url: string
+    name: string
+    login: string
+  }
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null)
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=9774b7e62b28262a37d0`
+
+  const signIn = async (githubCode: string) => {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode
+    })
+
+    const { token, user } = response.data
+
+    localStorage.setItem('@nlw:token', token)
+
+    setUser(user)
+  }
+
+  const signOut = () => {
+    setUser(null)
+    localStorage.removeItem('@nlw:token')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@nlw:token')
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`
+
+      api.get<User>('profile').then((response) => {
+        setUser(response.data)
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const url = window.location.href
+    const hasGithubCode = url.includes('?code=')
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode)
+
+      signIn(githubCode)
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, signInUrl, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
