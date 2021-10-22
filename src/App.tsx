3aa2be3f0e3678191808a6bb7bfc7e9b809d9b01
Andre <afelipp3@gmail.com>
2021-10-22 16:16:47 -0300

    Create the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useContext } from 'react'
+import { LoginBox } from './components/LoginBox'
+import { MessageList } from './components/MessageList'
+import { AuthContext } from './contexts/AuthContext'
+
+import styles from './App.module.scss'
+import { SendMessageForm } from './components/SendMessageForm'
+
 function App() {
+  const { user } = useContext(AuthContext)
   return (
-    <div className="App">
-      <h1>APP</h1>
-    </div>
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ''
+      }`}
+    >
+      <MessageList />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
   )
 }
 
